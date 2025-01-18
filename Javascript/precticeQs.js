512f5b1b529144fb6b17eab07ac713765a9f0495
Engineeer-ravi <pathakravi.er@gmail.com>
2025-01-18 23:06:54 +0530

    javascript

---
+++ Javascript/precticeQs.js
@@ -0,0 +1,34 @@
+// templet littrls
+// let applePrice=50;
+// let bananaPrice=60;
+// let output="The Total  Price is:"+(applePrice+bananaPrice)+"Rupes.";
+// let output=`The Total Price is :${applePrice+bananaPrice} Rupese.`;
+
+// console.log(output)
+
+// opretor in js
+let x = 5;
+
+// addition operator
+console.log("Addition: x + 3 = ", x + 3);
+
+// subtraction operator
+console.log("Subtraction: x - 3 =", x - 3);
+
+// multiplication operator
+console.log("Multiplication: x * 3 =", x * 3);
+
+// division operator
+console.log("Division: x / 3 =", x / 3);
+
+// remainder operator
+console.log("Remainder: x % 3 =", x % 3);
+
+// increment operator
+console.log("Increment: ++x =", ++x);
+
+// decrement operator
+console.log("Decrement: --x =", --x);
+
+// exponentiation operator
+console.log("Exponentiation: x ** 3 =", x ** 3);
