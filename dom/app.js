8000e1b90c47a37ba5e9666740cdc66bea0c30c5
Engineeer-ravi <pathakravi.er@gmail.com>
2025-01-31 19:01:41 +0530

    code

---
+++ dom/app.js
@@ -0,0 +1,34 @@
+
+// let smallImages = document.getElementsByClassName("oldImg");
+
+// for(let i=0; i<smallImages.length; i++){
+//     smallImages[i].src = "spider.jpg";                       "spider.jpg"
+//     console.log(`value of image no. ${i}is changed.`);
+// }
+
+
+//Selecting Element by Tag Name
+//document.getElementsByTagName("img");
+
+//Queery Selectors 
+//allow us to use any css Selector
+
+//  console.dir(document.querySelector('h1'));
+//  console.dir(document.querySelector('#description'));
+//  console.dir(document.querySelector('.oldImg'));
+
+// console.dir(document.querySelector('div a'));
+
+
+// console.dir(document.querySelectorAll('div a'));
+
+// console.dir(document.querySelectorAll('p'));
+ 
+
+
+
+
+//using propartise  & methode
+//innerText- Show  the visible text contained in a node
+//textContent- Show all the  full text
+//innerHTML-show the full markup
