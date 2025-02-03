c2c9759682029ec2e805924261aede42dcebccba
Engineeer-ravi <pathakravi.er@gmail.com>
2025-02-03 20:58:13 +0530

    code

--- dom/domEvent/Script.js
+++ dom/domEvent/Script.js
@@ -46,8 +46,70 @@
 
 // this in event listeners 
 // when 'this' is used in a callback of event handeler of something it refers to that something
+// let btn = document.querySelector("button");
+// let p = document.querySelector("p");
+// let h1 = document.querySelector("h1");
+// let h3 = document.querySelector("h3");
+
+// function changeColor(){
+//     console.dir(this.innerText);
+//     this.style.backgroundColor ="red";
+    
+// }
+
+// btn.addEventListener("click",changeColor)
+
+// p.addEventListener("click",changeColor)
+
+
+// h1.addEventListener("click",changeColor)
+
+// h3.addEventListener("click",changeColor)
+
+
+
+
+
+//keybord Events
 let btn = document.querySelector("button");
 
-btn.addEventListener("click",function(){
-    console.log(this);
+// btn.addEventListener("click",function(event){
+//     console.log(event);
+//     console.log("button was clicked");
+// });
+
+// btn.addEventListener("dblclick",function(event){
+//     console.log(event);
+//     console.log("button was clicked");
+// })  ;
+  
+// let input = document.querySelector("input")
+
+// input.addEventListener("keydown",function(event){
+//     console.log(event);
+//     console.log("key was pressed");
+// }); 
+
+// input.addEventListener("keyup",function(){
+//     console.log("key was relesed");
+// }); 
+
+
+//form Event listener
+let form = document.querySelector("form");
+
+form.addEventListener("submit",function(event){
+    event.preventDefault();
+    // alert ("form was submitted");
+
+    let input = document.querySelector("input");
+    console.log(input); 
+    console.dir(input.innerText); 
+    console.log(input.value)
 });
+
+//extracting data from form
+
+//change event
+
+ //??  
