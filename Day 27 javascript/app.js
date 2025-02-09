23302c9342e3b7ccda42442b8718ae664206f2a6
Engineeer-ravi <pathakravi.er@gmail.com>
2025-02-09 20:34:34 +0530

    git

--- Day 27 javascript/app.js
+++ Day 27 javascript/app.js
@@ -61,35 +61,39 @@
 // }
 
 //Axios - updating Query String
-let url ="http://universities.hipolabs.com/search?name=";
-let btn =document.querySelector("button");
+// let url ="http://universities.hipolabs.com/search?name=";
+// let btn =document.querySelector("button");
 
-btn.addEventListener("click",async () => {
-    let country = document.querySelector("input") .value;
-    console.log(country);
-     let colArr = await getColleges(country);
-     show(colArr);
-});
+// btn.addEventListener("click",async () => {
+//     let country = document.querySelector("input") .value;
+//     console.log(country);
+//      let colArr = await getColleges(country);
+//      show(colArr);
+// });
 // let country ="nepal";
 
-function show(colArr){
-    let list = document.querySelector("#list");
-    list.innerText = "";
-    for(col of colArr){
-        console.log(col.name);
+// function show(colArr){
+//     let list = document.querySelector("#list");
+//     list.innerText = "";
+//     for(col of colArr){
+//         console.log(col.name);
 
-        let li = document.createElement("li");
-        li.innerText =col.name;
-        list.appendChild(li);
-    }
+//         let li = document.createElement("li");
+//         li.innerText =col.name;
+//         list.appendChild(li);
+//     }
 
-}
-async function getColleges(country){
-    try{
-        let res = await axios.get(url+country);
-        return res.data;
-        }catch (err){
-            console.log("error",err);
-            return [];
-    }
-}
+// }
+// async function getColleges(country){
+//     try{
+//         let res = await axios.get(url+country);
+//         return res.data;
+//         }catch (err){
+//             console.log("error",err);
+//             return [];
+//     }
+// }
+
+
+
+console.log("Git Push")
