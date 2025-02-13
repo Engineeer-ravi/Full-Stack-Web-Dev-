d3642022713885384a1201f3b31f89dccf234332
Engineer-ravi <pathakravi.er@gmail.com>
2025-02-13 20:29:47 +0530

    Bakend

---
+++ Backend/script.js
@@ -0,0 +1,16 @@
+
+
+let n=5
+for(let i=1; i<=n; i++){
+    console.log("Hello" ,i);
+}
+
+console.log("bye")
+
+
+// console.log(process.argv);
+
+// const someValue = require("./math");
+
+
+// console.log(someValue);
