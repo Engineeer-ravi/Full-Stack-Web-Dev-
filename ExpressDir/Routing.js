f60f886c6c50fd917b0950d3375cac4e493d3c60
Engineer-ravi <pathakravi.er@gmail.com>
2025-02-20 19:25:53 +0530

    request response

---
+++ ExpressDir/Routing.js
@@ -0,0 +1,3 @@
+
+// Routing--it is process of selecting a path for traffic in a network or between or  across multiple networks
+
--- ExpressDir/index.js
+++ ExpressDir/index.js
@@ -10,3 +10,24 @@ app.listen(port, () => {
    console.log(`app is  listen on port ${port}`);
 
 } );
+ 
+//handling request
+
+
+app.use((req,res) => {
+   // console.log(req);
+   console.log("request recived");
+
+   // // res.send("this is a basic respond");
+
+   // //object respond
+   // res.send({
+   //    name:"apple",
+   //    color:"red",
+   // });
+
+   //html respond
+   let code = "<h1>Fruits</h1> <ul><li>Apple</li><li>orange</li></ul>";
+   res.send(code);
+
+});
