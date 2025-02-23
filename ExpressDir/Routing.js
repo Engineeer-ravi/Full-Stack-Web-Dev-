02562ee6030f810e85ca74cb43fbeae512f5a65f
Engineer-ravi <pathakravi.er@gmail.com>
2025-02-23 19:08:20 +0530

    expressEnd

--- ExpressDir/Routing.js
+++ ExpressDir/Routing.js
@@ -1,3 +1,35 @@
 
 // Routing--it is process of selecting a path for traffic in a network or between or  across multiple networks
 
+const express = require("express");
+const app = express();
+
+// console.dir(app);
+
+let port = 3000; //8080
+
+app.listen(port, () => {
+   console.log(`app is  listen on port ${port}`);
+
+} );
+  
+
+  app.get("/apple", (req,res) => {
+    res.send("you contacted apple path");
+  });
+
+  app.get("/orange", (req,res) => {
+    res.send("you contacted orange path");
+  });
+
+  app.get("*", (req,res) => {
+    res.send("this path does not exists");
+  });
+
+  //app.post methode
+
+  app.post("/", (req,res) => {
+    res.send("you sent a post request to root");
+  });
+
+  //NodeMon-- to autometically restart server with code chage
--- ExpressDir/index.js
+++ ExpressDir/index.js
@@ -14,9 +14,24 @@ app.listen(port, () => {
 //handling request
 
 
-app.use((req,res) => {
-   // console.log(req);
-   console.log("request recived");
+// app.use((req,res) => {
+//    // console.log(req);
+//    console.log("request recived");
+
+   
+  app.get("/", (req,res) => {
+   res.send("Hello i am root ravi pathak");
+ });
+
+ app.get("/:username/:id", (req,res) => {
+   // console.log(req.params);
+   let{username,id} = req.params;
+   //Html send 
+   let htmlStr =`<h1>welcome to the page of @${username}</h1>`;
+   res.send(htmlStr);
+   // res.send(`welcome to the page of @${username}`);
+ });
+
 
    // // res.send("this is a basic respond");
 
@@ -27,7 +42,17 @@ app.use((req,res) => {
    // });
 
    //html respond
-   let code = "<h1>Fruits</h1> <ul><li>Apple</li><li>orange</li></ul>";
-   res.send(code);
+//    let code = "<h1>Fruits</h1> <ul><li>Apple</li><li>orange</li></ul>";
+//    res.send(code);
 
+// });
+//Query String
+app.get("/search", (req,res) => {
+   // console.log(req.query)
+   let {q} = req.query;
+   if(!q){
+      res.send("<h1>nothing searched</h1>");
+   }
+
+   res.send(`<h1>search result for query : ${q}</h1>`);
 });
