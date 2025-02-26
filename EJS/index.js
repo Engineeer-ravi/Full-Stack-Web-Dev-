9f34c80ee5db386d83f4368e0dc18abad66ad013
Engineer-ravi <pathakravi.er@gmail.com>
2025-02-26 22:30:47 +0530

    code

--- EJS/index.js
+++ EJS/index.js
@@ -15,11 +15,22 @@ app.set("views",path.join(__dirname,"views"));
 app.get("/",(req,res) => {
     res.render("home.ejs");
 });
+//instagram Ejs use
+
+app.get("/ig/:username",(req,res) => {
+    const followers =["mithlesh","raviranjan","deepak","rohit","payal"];
+    let {username} = req.params;
+    res.render("instagram.ejs",{username, followers});
+});
+
+//random number Ejs use
 
 app.get("/rolldice",(req,res) => {
     let diceVal = Math.floor(Math.random()*6) +1;
     res.render("rolldice.ejs", { diceVal });
 });
+
+
 
 
 app.listen(port,() =>{
