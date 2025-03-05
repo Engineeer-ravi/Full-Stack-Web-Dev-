b40b01ee4b7fc5eb6b6f004ddddb31293fb5b1c8
Engineer-ravi <pathakravi.er@gmail.com>
2025-03-05 22:40:50 +0530

    code

--- Rest-Class/index.js
+++ Rest-Class/index.js
@@ -5,15 +5,16 @@ const port = 8080;
 const path = require("path");
 
 app.use(express.urlencoded({ extended : true}));
+app.use(express.json());
 
 
 
 app.set("view engine", "ejs");
-app.set("view", path.join(__dirname,"views"));
+app.set("views", path.join(__dirname,"views"));
 
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-let post = [
+let posts = [
     {
         username : "codebyravi",
         content : "I Love coding!"
@@ -29,9 +30,24 @@ let post = [
 ];
 
 
-
+//posts route API
 app.get("/posts", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs",{ posts: posts });
+});
+
+// add a new post
+app.get("/posts/new", (req,res) => {
+    res.render("new.ejs");
+});
+
+app.post("/posts", (req,res) =>  {
+    // console.log(req.body);
+    
+    let { username,content } = req.body;
+    posts.push({ username, content });
+    // res.send("Post request to /posts");
+    res.redirect("/posts");
+   
 });
 
 app.listen(port, () => {
